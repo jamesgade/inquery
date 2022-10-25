e2a04cdc75787c906c4a27278366cddbd3582ab2
jamesleaf <gadejames48@gmail.com>
2022-10-25 11:57:31 +0530

    db connect, colors, errorHandler

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
+
+// connect to database
+connectDB()
 
 const app = express()
 
+// middleware to receive raw/json data in req
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
-    res.status(200).json({mesage: "Welcome to Inquery-API"})
+    res.status(200).json({ mesage: "Welcome to Inquery-API" })
 })
 
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`server started on port : ${PORT}`))
