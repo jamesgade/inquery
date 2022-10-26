7832a7e4198709dcd4a3837d4cdd437034be79c8
jamesleaf <gadejames48@gmail.com>
2022-10-26 13:19:50 +0530

    backend: tickets, get & create

---
+++ backend/controllers/ticketController.js
@@ -0,0 +1,56 @@
+const asyncHandler = require('express-async-handler')
+
+const User = require('../models/userModel')
+const Ticket = require('../models/ticketModel')
+
+// @desc    get user tickets
+// @route   GET /api/tickets
+// @access  private
+const getTickets = asyncHandler(async (req, res) => {
+
+    // get user from id in jwt token
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const tickets = await Ticket.find({ user: req.user.id })
+
+    res.status(200).json(tickets)
+})
+
+// @desc    get user tickets
+// @route   POST /api/tickets
+// @access  private
+const createTicket = asyncHandler(async (req, res) => {
+
+    const { product, description } = req.body
+
+    if (!product || !description) {
+        res.status(400)
+        throw new Error('Please add a product and description')
+    }
+
+    // get user from id in jwt token
+    const user = await User.findById(req.user.id)
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user: req.user.id,
+        // status: 'new'   // default
+    })
+
+    res.status(201).json(ticket)
+})
+
+module.exports = {
+    getTickets,
+    createTicket
+}
