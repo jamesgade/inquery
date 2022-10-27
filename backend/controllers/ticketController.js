5db929fde1e0bae982f7ba186c0b0accafa47f50
jamesleaf <gadejames48@gmail.com>
2022-10-27 09:51:03 +0530

    backend: tickets crud ops

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -4,24 +4,6 @@ const User = require('../models/userModel')
 const Ticket = require('../models/ticketModel')
 
 // @desc    get user tickets
-// @route   GET /api/tickets
-// @access  private
-const getTickets = asyncHandler(async (req, res) => {
-
-    // get user from id in jwt token
-    const user = await User.findById(req.user.id)
-
-    if (!user) {
-        res.status(401)
-        throw new Error('User not found')
-    }
-
-    const tickets = await Ticket.find({ user: req.user.id })
-
-    res.status(200).json(tickets)
-})
-
-// @desc    get user tickets
 // @route   POST /api/tickets
 // @access  private
 const createTicket = asyncHandler(async (req, res) => {
@@ -50,7 +32,111 @@ const createTicket = asyncHandler(async (req, res) => {
     res.status(201).json(ticket)
 })
 
+// @desc    get user tickets
+// @route   GET /api/tickets
+// @access  private
+const getTickets = asyncHandler(async (req, res) => {
+
+    // get user from id in jwt token
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const tickets = await Ticket.find({ user: req.user.id })
+
+    res.status(200).json(tickets)
+})
+
+// @desc    get user's single ticket by id
+// @route   GET /api/tickets/:id
+// @access  private
+const getTicket = asyncHandler(async (req, res) => {
+
+    // get user from id in jwt token
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.id)
+
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket Not found')
+    }
+
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+
+    res.status(200).json(ticket)
+})
+
+// @dedc    delete a ticket
+// route    DELETE /api/tickets/:id
+// @access  private
+const deleteTicket = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.id)
+
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+
+    await ticket.remove()
+
+    res.status(200).json({ success: true })
+})
+
+// @desc    update ticket
+// @route   PUT /api/tickets/:id
+// @access  private
+const updateTicket = asyncHandler(async (req, res) => {
+
+    //check user
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.id)
+
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json(updatedTicket)
+
+})
+
+
 module.exports = {
+    createTicket,
     getTickets,
-    createTicket
+    getTicket,
+    deleteTicket,
+    updateTicket
 }
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { getTickets, createTicket } = require('../controllers/ticketController')
+const { getTickets, createTicket, getTicket, updateTicket, deleteTicket } = require('../controllers/ticketController')
 
 const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(protect, getTickets).post(protect, createTicket)
+router.route('/').get(protect, getTickets)
+router.route('/create').post(protect, createTicket)
+router.route('/:id').get(protect, getTicket)
+router.route('/:id').put(protect, updateTicket)
+router.route('/:id').delete(protect, deleteTicket)
 
 module.exports = router
